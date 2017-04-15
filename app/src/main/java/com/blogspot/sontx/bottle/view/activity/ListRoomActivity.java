920de8ed1c1309fbc8bbc4d945cf64b0e61f1646
sontx <xuanson33bk@gmail.com>
2017-04-15 16:40:23 +0700

    + select another category

--- app/src/main/java/com/blogspot/sontx/bottle/view/activity/ListRoomActivity.java
+++ app/src/main/java/com/blogspot/sontx/bottle/view/activity/ListRoomActivity.java
@@ -6,15 +6,25 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.blogspot.sontx.bottle.R;
 import com.blogspot.sontx.bottle.model.bean.Category;
 import com.blogspot.sontx.bottle.model.bean.Room;
+import com.blogspot.sontx.bottle.presenter.CategoryPresenterImpl;
+import com.blogspot.sontx.bottle.presenter.interfaces.CategoryPresenter;
 import com.blogspot.sontx.bottle.utils.StringUtils;
+import com.blogspot.sontx.bottle.view.adapter.CategoryAdapter;
 import com.blogspot.sontx.bottle.view.fragment.ListRoomFragment;
+import com.blogspot.sontx.bottle.view.interfaces.ListCategoryView;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnItemClickListener;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import butterknife.BindDimen;
 import butterknife.BindView;
@@ -23,10 +33,12 @@ import butterknife.ButterKnife;
 /**
  * Provide either room id or category id. If provide room id, api will fetch category id first.
  */
-public class ListRoomActivity extends ActivityBase implements ListRoomFragment.OnListRoomInteractionListener {
+public class ListRoomActivity extends ActivityBase implements ListRoomFragment.OnListRoomInteractionListener, ListCategoryView {
 
     public static final String ROOM_ID = "room-id";
     public static final String CATEGORY_ID = "category-id";
+
+    private CategoryPresenter categoryPresenter;
 
     @BindView(R.id.collapsing_toolbar)
     CollapsingToolbarLayout collapsingToolbarLayout;
@@ -42,6 +54,13 @@ public class ListRoomActivity extends ActivityBase implements ListRoomFragment.O
         setupToolbar();
         ButterKnife.bind(this);
         processArguments();
+        categoryPresenter = new CategoryPresenterImpl(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.select_category, menu);
+        return true;
     }
 
     @Override
@@ -52,6 +71,12 @@ public class ListRoomActivity extends ActivityBase implements ListRoomFragment.O
             finish();
             return true;
         }
+
+        if (item.getItemId() == R.id.menu_item_category) {
+            categoryPresenter.getCategoriesAsync();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -110,5 +135,22 @@ public class ListRoomActivity extends ActivityBase implements ListRoomFragment.O
             int width = headerImageView.getWidth();
             Picasso.with(this).load(category.getPhotoUrl()).resize(width, previewHeight).centerCrop().into(headerImageView);
         }
+    }
+
+    @Override
+    public void showCategories(List<Category> result) {
+        DialogPlus dialogPlus = DialogPlus.newDialog(this)
+                .setAdapter(new CategoryAdapter(result))
+                .setExpanded(true)
+                .setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(DialogPlus dialog, Object item, View view, int position) {
+                        headerImageView.setImageResource(R.drawable.default_cover);
+                        placeListRoomFragmentByCategoryId(((Category) item).getId());
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        dialogPlus.show();
     }
 }
---
+++ app/src/main/java/com/blogspot/sontx/bottle/view/adapter/CategoryAdapter.java
@@ -0,0 +1,75 @@
+package com.blogspot.sontx.bottle.view.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.blogspot.sontx.bottle.R;
+import com.blogspot.sontx.bottle.model.bean.Category;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import lombok.Getter;
+
+public class CategoryAdapter extends BaseAdapter {
+
+    @Getter
+    private List<Category> values;
+
+    public CategoryAdapter(List<Category> items) {
+        values = items;
+    }
+
+    @Override
+    public int getCount() {
+        return values.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return values.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        Category category = values.get(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.item = category;
+
+        viewHolder.titleView.setText(category.getName());
+        viewHolder.descriptionView.setText(category.getDescription());
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        final View root;
+        final TextView titleView;
+        final TextView descriptionView;
+
+        Category item;
+
+        ViewHolder(View view) {
+            root = view;
+            titleView = ButterKnife.findById(view, R.id.title_view);
+            descriptionView = ButterKnife.findById(view, R.id.description_view);
+        }
+    }
+}
